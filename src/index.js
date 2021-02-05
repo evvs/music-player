3a7c153d13fa5f37f6de5cbd9065d4baa2d88489
evvs <va.evgeny@gmail.com>
2021-02-05 19:52:19 +0300

    play / pause on spacebar

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import playlistReducer from './redux_slices/playlist';
-import currentSongReducer from './redux_slices/currentSong';
+import currentSongReducer, { playToggle } from './redux_slices/currentSong';
 import libriaryReducer from './redux_slices/libriary';
 import playlistData from './data';
 import App from './App.jsx';
@@ -27,3 +27,9 @@ render(
   </Provider>,
   document.getElementById('root'),
 );
+
+document.addEventListener('keyup', (event) => {
+  if (event.code === 'Space') {
+    store.dispatch(playToggle());
+  }
+});
